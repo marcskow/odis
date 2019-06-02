3f165a81fb1220b6232095623afd1bf71f1a776c
Marcin Skowron <mskowron17@gmail.com>
2019-06-02 16:47:55 +0200

    Better brute force protection

--- abruteforce.sh
+++ abruteforce.sh
@@ -12,7 +12,6 @@ iptables -A INPUT -s 184.254.213.0/24 -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -s 185.254.214.200 -p tcp --dport 22 -j ACCEPT
 
 # ewentualnie (gdybyśmy nie chcieli wprowadzać powyżej whitelisty, tylko umożliwić połączenia z dowolnego adresu) możemy wprowadzić limit połączeń także dla SSH (w konfiguracji anty DDoS wprowadziliśmy limit połączeń dla HTTP i HTTPS, w związku z tym nasze aplikacje są odpowiednio chronione, ponadto stosujemy w nich “wolny” algorytmy kryptograficzny bcrypt, który skutecznie spowalnia łamanie haseł metodą brute force, jednak jeśli chcemy dodatkowym zabezpieczeniem objąć również SSH, należy wprowadzić connection limit jak niżej)
-# iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --set
-# iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
-
-iptables -A INPUT -p tcp -m state --state NEW -j DROP
+iptables -A INPUT -p tcp -m tcp --dport 22 -m state --state NEW -m recent --set --name DEFAULT --rsource
+iptables -A INPUT -p tcp -m tcp --dport 22 -m state --state NEW -m recent --update --seconds 180 --hitcount 4 --name DEFAULT --rsource -j DROP
+iptables -A INPUT -p tcp -m state --state NEW --dport 22 -j ACCEPT
