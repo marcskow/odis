b9c4440c879253a5fe2966ec624b8bf7a421c1ed
Marcin Skowron <mskowron17@gmail.com>
2019-06-02 14:16:26 +0000

    Fix bruteforce

--- abruteforce.sh
+++ abruteforce.sh
@@ -3,7 +3,7 @@
 # usuwamy komendę, która ma dropować wszystkie zapytania, których nie obsłużyliśmy do tej pory, ponieważ chcemy dokleić nowe
 # reguły, jeśli odpalamy ten skrypt jako jedyny, nie musielibyśmy tego robić, możemy otrzymać komunikat że nie ma reguły
 # którą usuwamy, aczkolwiek w razie gdyby istniała już inna konfiguracja, przygotowujemy się na taką ewentualność
-iptables -D INPUT -p tcp -m state --state NEW DROP
+iptables -D INPUT -p tcp -m state --state NEW -j DROP
 
 # pozwalamy na dostęp przez SSH z sieci wewnętrznej (dla celów administracyjnych)
 iptables -A INPUT -s 184.254.213.0/24 -p tcp --dport 22 -j ACCEPT
@@ -12,7 +12,7 @@ iptables -A INPUT -s 184.254.213.0/24 -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -s 185.254.214.200 -p tcp --dport 22 -j ACCEPT
 
 # ewentualnie (gdybyśmy nie chcieli wprowadzać powyżej whitelisty, tylko umożliwić połączenia z dowolnego adresu) możemy wprowadzić limit połączeń także dla SSH (w konfiguracji anty DDoS wprowadziliśmy limit połączeń dla HTTP i HTTPS, w związku z tym nasze aplikacje są odpowiednio chronione, ponadto stosujemy w nich “wolny” algorytmy kryptograficzny bcrypt, który skutecznie spowalnia łamanie haseł metodą brute force, jednak jeśli chcemy dodatkowym zabezpieczeniem objąć również SSH, należy wprowadzić connection limit jak niżej)
-iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --set
-iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
+# iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --set
+# iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
 
-iptables -A INPUT -p tcp -m state --state NEW DROP
+iptables -A INPUT -p tcp -m state --state NEW -j DROP
