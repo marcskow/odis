4e952ab6498309e201b2e972971d720bed411a56
Marcin Skowron <mskowron17@gmail.com>
2019-06-07 00:37:31 +0200

    Add port and protocol if needed, trying with - check if rule already exists

--- active-firewall/start.py
+++ active-firewall/start.py
@@ -14,12 +14,36 @@ OPTIONAL_PORT_REGEX = r"(:\d{1,5})?"
 IP_PORT_REGEX = IP_REGEX + OPTIONAL_PORT_REGEX
 
 # constants
-ICMP_PING_NMAP_LIFETIME = 5
+DEFAULT_RULE_LIFETIME = 5
+CLEANER_INTERVAL = 20
 
 
 # implementation
+class Rule:
+    def __init__(self, protocol="tcp", lifetime=DEFAULT_RULE_LIFETIME):
+        self.protocol = protocol
+        self.lifetime = lifetime
+
+    def as_iptables_entry(self, ip):
+        port = f" --dport {ip.port}" if ip.port else ""
+
+        return f"INPUT" \
+            f" -p {self.protocol}" \
+            f"{port}" \
+            f" -s {ip.address} -j DROP"
+
+
+class Ip:
+    def __init__(self, address, port):
+        self.address = address
+        self.port = port
+
+    def __str__(self) -> str:
+        return f"{self.address}" + f":{self.port}" if self.port else ""
+
+
 class ActiveFirewallConfiguration:
-    def __init__(self, attack, message, rule):
+    def __init__(self, attack, message, rule=Rule()):
         self.attack = attack
         self.message = message
         self.rule = rule
@@ -36,17 +60,29 @@ entryQueue = queue.Queue()
 configurations = []
 
 
+# def check_if_already_exists(ip):
+#     proc = subprocess.Popen(['iptables-save', 'fake_utility.py'], stdout=subprocess.PIPE)
+#     while True:
+#         line = proc.stdout.readline()
+#         if not line:
+#             break
+#         if ip.port:
+#             re.search()
+
+
 def source_post_process(source):
-    return re.search(IP_REGEX, source).group()
+    ip = re.search(IP_REGEX, source).group()
+    port = re.search(OPTIONAL_PORT_REGEX, source).group().replace(':', '')
+    return Ip(ip, port)
 
 
 def detect(message):
     m = re.search(IP_PORT_REGEX + " -> " + IP_PORT_REGEX, message)
     if m is not None:
         print(m.group())
-        source = m.group().split(" -> ")[0]
-        pp = source_post_process(source)
-        return pp
+        source_destination = m.group().split(" -> ")[0]
+        source = source_post_process(source_destination)
+        return source
 
 
 def process(line):
@@ -60,8 +96,8 @@ def process(line):
 
         print(f"Attacker address: {source}")
 
-        rule = match.rule.format(source)
-        entryQueue.put(IpTablesEntry(rule, datetime.datetime.now(), ICMP_PING_NMAP_LIFETIME))
+        rule = match.rule.as_iptables_entry(source)
+        entryQueue.put(IpTablesEntry(rule, datetime.datetime.now(), match.rule.lifetime))
 
         print(f"Adding /sbin/iptables -A {rule}")
         os.system(f"/sbin/iptables -A {rule}")
@@ -77,12 +113,28 @@ def cleaner():
             else:
                 entryQueue.put(entry)
                 break
-        time.sleep(20)
+        time.sleep(CLEANER_INTERVAL)
 
 
-configurations.append(ActiveFirewallConfiguration("ICMP PING NMAP", "Port scanning detected.", "INPUT -s {} -j DROP"))
-configurations.append(ActiveFirewallConfiguration("Possible TCP DoS", "TCP DoS detected.", "INPUT -s {} -j DROP"))
-configurations.append(ActiveFirewallConfiguration("Ping of Death Detected", "Ping of Death Detected.", "INPUT -s {} -j DROP"))
+configurations.append(
+    ActiveFirewallConfiguration(
+        "ICMP PING NMAP",
+        "Port scanning detected.",
+        Rule(protocol="icmp", lifetime=10)
+    )
+)
+configurations.append(
+    ActiveFirewallConfiguration(
+        "Possible TCP DoS",
+        "TCP DoS detected."
+    )
+)
+configurations.append(
+    ActiveFirewallConfiguration(
+        "Ping of Death Detected",
+        "Ping of Death Detected."
+    )
+)
 
 for stdLine in sys.stdin:
     thread = threading.Thread(target=cleaner, args=())
